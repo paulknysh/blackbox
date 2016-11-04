b8dd5ba1aec1ca301f86ded01f6543b9b81767f2
lancastdw <lancastdw@gmail.com>
2016-11-03 20:48:42 -0400

    Update example.py

--- example.py
+++ example.py
@@ -2,14 +2,21 @@
 """Example from arXiv note (https://arxiv.org/pdf/1605.00998.pdf)"""
 import blackbox as bb
 
+
 def fun(par):
     x = par[0]
     y = par[1]
     return abs(x-y) + ((x+y-1)/3)**2
 
-bb.search(f=fun,
-          box=[[-1.,1.],[-1.,1.]],
-          n=8,
-          it=8,
-          cores=1,
-          resfile='output.csv')
+
+def main()
+    bb.search(f=fun,
+              box=[[-1.,1.],[-1.,1.]],
+              n=8,
+              it=8,
+              cores=2,
+              resfile='output.csv')
+
+    
+if __name__ == '__main.py__':
+    main()
