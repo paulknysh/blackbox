aeb3ebfe51f64899ba7910772994974ad49a534a
Paul Knysh <paul.knysh@gmail.com>
2019-02-27 19:10:48 -0500

    Replaced latin hypercube with R-sequence

--- blackbox.py
+++ blackbox.py
@@ -83,9 +83,9 @@ def search(f, box, n, m, batch, resfile,
     def cubetobox(x):
         return [box[i][0]+(box[i][1]-box[i][0])*x[i] for i in range(d)]
 
-    # generating latin hypercube
+    # generating R-sequence
     points = np.zeros((n, d+1))
-    points[:, 0:-1] = latin(n, d)
+    points[:, 0:-1] = rseq(n, d)
 
     # initial sampling
     for i in range(n//batch):
@@ -145,9 +145,9 @@ def search(f, box, n, m, batch, resfile,
     np.savetxt(resfile, points, delimiter=',', fmt=' %+1.4e', header=''.join(labels), comments='')
 
 
-def latin(n, d):
+def rseq(n, d):
     """
-    Build latin hypercube.
+    Build R-sequence (http://extremelearning.com.au/unreasonable-effectiveness-of-quasirandom-sequences/).
 
     Parameters
     ----------
@@ -158,33 +158,18 @@ def latin(n, d):
 
     Returns
     -------
-    lh : ndarray
+    points : ndarray
         Array of points uniformly placed in d-dimensional unit cube.
     """
-    def phi(d):
-        x = 2
-        for i in range(10):
-            x = pow( 1+x, 1/(d+1) )
-        return x
+    phi = 2
+    for i in range(10):
+        phi = pow(1+phi, 1./(d+1))
 
-    g = phi(d)
+    alpha = np.array([pow(1./phi, i+1) for i in range(d)])
 
-    alpha = np.zeros(d)
-    for j in range(d):
-        alpha[j] = pow(1/g, j+1)
-    alpha = alpha % 1
+    points = np.array([(0.5 + alpha*(i+1)) % 1 for i in range(n)])
 
-    # This number can be any real number.
-    # Common default setting is typically seed=0
-    # But seed = 0.5 is generally better.
-    seed = np.random.rand()
-
-    lh = np.zeros((n, d))
-    for i in range(n):
-        lh[i] = seed + alpha*(i+1)
-    lh = lh % 1
-
-    return lh
+    return points
 
 
 def rbf(points, T):
