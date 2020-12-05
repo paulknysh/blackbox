d69cb0ffdb3ef4698c9c72134e728176b96157fe
Paul Knysh <paul.knysh@gmail.com>
2020-12-05 23:18:08 +0000

    packaged blackbox (pypi)

---
+++ black_box/__init__.py
@@ -0,0 +1,1 @@
+from .blackbox import *
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="black_box",
+    version="1.0.0",
+    author="Paul Knysh",
+    author_email="paul.knysh@gmail.com",
+    description="A Python module for parallel optimization of expensive black-box functions",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/paulknysh/blackbox",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
