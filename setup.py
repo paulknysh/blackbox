96fb9bd3895f666c8a02c5a052293af3a3ae944e
Paul Knysh <paul.knysh@gmail.com>
2020-12-06 00:54:09 +0000

    added required libraries to setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="black_box",
-    version="1.0.0",
+    version="1.0.2",
     author="Paul Knysh",
     author_email="paul.knysh@gmail.com",
     description="A Python module for parallel optimization of expensive black-box functions",
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/paulknysh/blackbox",
     packages=setuptools.find_packages(),
+    install_requires=['numpy', 'scipy'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
